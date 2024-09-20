ef825b015ad83d9b27bfa4ad7be00cd9c7d16fc6
hshaw412 <harvey.shaw@griffithuni.edu.au>
2024-09-20 10:13:06 +1000

    Added interaction with UI and question gen server

--- QuizMessAround/script.js
+++ QuizMessAround/script.js
@@ -1,3 +1,12 @@
+let questions;
+
+(async function() {
+    await fetchData();
+
+    console.log("You can now use these questions:", questions);
+    console.log(typeof questions)
+})();
+
 const startButton = document.getElementById('start-btn')
 const nextButton = document.getElementById('next-btn')
 const quizContainer = document.getElementById('question-container')
@@ -7,6 +16,19 @@ let questionsShuffled, currentQuestionIndex
 const questionElement = document.getElementById('question')
 const answerButtonsElement = document.getElementById('answer-btns')
 
+async function fetchData() {
+    try {
+        const response = await fetch("http://127.0.0.1:5000/get-questions");
+        if (!response.ok) {
+            throw new Error("Network response was not ok");
+        }
+        questions = await response.json();
+        questions = questions.questions
+        console.log("Questions set:", questions);
+    } catch(error) {
+        console.error('There was a problem with the fetch operation:', error);
+    }
+}
 
 startButton.addEventListener('click', startQuiz)
 nextButton.addEventListener('click', () => {
@@ -18,6 +40,9 @@ function startQuiz(){
     //console.log('Quiz started')
     startButton.classList.add('hide')
     questionsShuffled = questions.sort(() => Math.random() - 0.5)
+    questionsShuffled.forEach(question => {
+        question.answers = question.answers.sort(() => Math.random() - 0.5)
+    })
     currentQuestionIndex = 0
     quizContainer.classList.remove('hide')
     setNextQuestion()
@@ -84,71 +109,3 @@ function getRandomInt(min, max) {
     const maxFloored = Math.floor(max);
     return Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled); // The maximum is exclusive and the minimum is inclusive
   }
-
-const questions = [
-    {//name of restaurant and corrent year need to be generated from datatbase
-        question: "When was Restaurant Opened?",
-        answers: [
-            { text: 'correct year', correct: true },
-            { text: getRandomInt(1973, 2024)
-                , correct: false },
-            { text: getRandomInt(1973, 2024)
-                , correct: false },
-            { text: getRandomInt(1973, 2024)
-                , correct: false }
-        ]
-    }, 
-    {
-        question: "What is Restaurant's main cuisine style?",
-        answers: [
-            {text: 'Italian', correct: false}, 
-            {text: 'Greek', correct: false}, 
-            {text: 'correct Answer', correct: true}, 
-            {text: 'Asain Fusion', correct: false}
-        ]
-    },
-    {
-        question: "Which is a dish from our menu?",
-        answers: [
-            {text: 'Chicken Parmigana', correct: false}, 
-            {text: 'correct Answer', correct: true}, 
-            {text: 'Fettuccini Gamberi', correct: false}, 
-            {text: 'Combinbation Sea-food chow mein', correct: false} 
-        ]
-    },
-    {
-        question: "How many vegetarian options do we offer on our menu?",
-        answers: [
-            {text: getRandomInt(1, 10), correct: false}, 
-            {text: getRandomInt(1, 10), correct: false}, 
-            {text: 'correct Answer', correct: true}, 
-            {text: getRandomInt(1, 10), correct: false}
-        ]
-    },
-    {
-        question: "Which of the following is one of our dessert specialties?",
-        answers: [
-            {text: 'Tiramisu', correct: false}, 
-            {text: 'correct Answer', correct: true}, 
-            {text: 'Chocolate Lava Cake', correct: false}, 
-            {text: 'Crème Brûlée', correct: false}
-        ]
-    },
-    {
-        question: "Which of the following dietary preferences does Restaurant cater to?",
-        answers: [
-            {text: 'Gluten-free', correct: false},
-            {text: 'Vegan', correct: false}, 
-            {text: 'correct Answer', correct: true},  
-            {text: 'All of the above', correct: false}
-        ]
-    },
-    {question: "When suburb is Restaurant located?",
-        answers: [ //generate random suburbs from city restaurant is located
-            {text: 'correct Suburb', correct: true}, 
-            {text: 'Suburb 1', correct: false},
-            {text: 'Suburb 2', correct: false},  
-            {text: 'Suburb 3', correct: false}
-        ]
-    }
-]

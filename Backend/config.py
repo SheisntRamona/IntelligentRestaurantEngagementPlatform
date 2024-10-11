2b8228225c0846b19cb2a2dad66f772dedbde5d8
hshaw412 <harvey.shaw@griffithuni.edu.au>
2024-10-11 18:22:08 +1000

    Changed database details to placeholders (FINAL COMMIT))

--- Backend/config.py
+++ Backend/config.py
@@ -5,7 +5,7 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
-app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://postgres:Mermoorkot14_2@localhost:5433/restaurant_data"
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://<USER>:<PASSWORD>@localhost:<PORT>/restaurant_data"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db = SQLAlchemy(app)
